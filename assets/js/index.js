5f79f51b50a98a24c91ec1ffdb3b680df2ccf0c7
KaranCitrusbug <karan.citrusbug@gmail.com>
2024-02-23 17:20:55 +0530

    Complete given changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
 const tabs = document.getElementById("tabs");
 const links = tabs.getElementsByClassName("links");
 
-for (var i = 0; i < links.length; i++) {
+for (let i = 0; i < links.length; i++) {
   links[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("activeTab");
     current[0].className = current[0].className.replace("activeTab", "");
@@ -134,10 +134,29 @@ function priorities(){
     prioritiesTable.style.display = "block";
   }
 }
+// -------------------------priorities section end------------
+
+// ---------kpi section start --------------------
 function kpiTable(){
   const arrow =document.querySelector(".arrow1")
   arrow.classList.toggle('rotated');
   const kpiTable = document.querySelector(".kpiTable")
+  const kpiHeader = document.querySelector(".kpiHeader")
+  if(arrow.classList.contains("rotated")){
+    kpiTable.style.display = "none";
+    kpiHeader.classList.add("space")
+  }
+  else{
+    kpiTable.style.display = "block";
+    kpiHeader.classList.remove("space")
+
+  }
+}
+function kpiTable2(){
+  const arrow =document.querySelector(".arrow2")
+  arrow.classList.toggle('rotated');
+  const kpiTable = document.querySelector(".kpiTable2")
+  const kpiHeader = document.querySelector(".kpiHeader")
   if(arrow.classList.contains("rotated")){
     kpiTable.style.display = "none";
   }
@@ -145,3 +164,115 @@ function kpiTable(){
     kpiTable.style.display = "block";
   }
 }
+// -----------------kpi section end ------------------------
+
+// --------------Feedback section start --------------------
+
+const feedbackYear = document.querySelectorAll(".year")
+for (let i = 0; i < feedbackYear.length; i++) {
+  feedbackYear[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("activeYear");
+    current[0].className = current[0].className.replace("activeYear", "");
+    this.className += " activeYear";
+  });
+}
+
+function feedbackResult(){
+  const arrow =document.querySelector(".arrow4")
+  arrow.classList.toggle('rotated');
+  const feedbackResult = document.querySelector(".feedbackResult")
+  if(arrow.classList.contains("rotated")){
+    feedbackResult.style.display = "none";
+
+  }
+  else{
+    feedbackResult.style.display = "flex";
+
+
+  }
+}
+function feedbackTableOne(){
+  const arrow =document.querySelector(".arrow3")
+  arrow.classList.toggle('rotated');
+  const feedbackTableOne = document.querySelector(".feedbackTableOne")
+  if(arrow.classList.contains("rotated")){
+    feedbackTableOne.style.display = "none";
+  }
+  else{
+    feedbackTableOne.style.display = "block";
+  }
+
+}
+function thirdQuarterTable(){
+  const arrow =document.querySelector(".arrow5")
+  arrow.classList.toggle('rotated');
+  const thirdQuarterDetails = document.querySelector(".thirdQuarterDetails")
+  if(arrow.classList.contains("rotated")){
+    thirdQuarterDetails.style.display = "none";
+  }
+  else{
+    thirdQuarterDetails.style.display = "flex";
+  }
+}
+
+function kpiResultTable(){
+  const arrow =document.querySelector(".arrow6")
+  arrow.classList.toggle('rotated');
+  const kpiResult = document.querySelector(".kpiResult")
+  if(arrow.classList.contains("rotated")){
+    kpiResult.style.display = "none";
+  }
+  else{
+    kpiResult.style.display = "block";
+  }
+}
+
+function kpiResultSales(){
+  const arrow =document.querySelector(".arrow7")
+  arrow.classList.toggle('rotated');
+  const salesTable = document.querySelector(".salesTable")
+  if(arrow.classList.contains("rotated")){
+    salesTable.style.display = "none";
+  }
+  else{
+    salesTable.style.display = "block";
+  }
+}
+
+function highImpactTable(){
+  const arrow =document.querySelector(".arrow8")
+  arrow.classList.toggle('rotated');
+  const highImpactTable = document.querySelector(".highImpactTable")
+  if(arrow.classList.contains("rotated")){
+    highImpactTable.style.display = "none";
+  }
+  else{
+    highImpactTable.style.display = "block";
+  }
+}
+function cultureFittable(){
+  const arrow =document.querySelector(".arrow9")
+  arrow.classList.toggle('rotated');
+  const cultureFittable = document.querySelector(".cultureFittable")
+  if(arrow.classList.contains("rotated")){
+    cultureFittable.style.display = "none";
+  }
+  else{
+    cultureFittable.style.display = "block";
+  }
+}
+
+// sidebar menu collapse when screen size above 1000px
+
+window.addEventListener("resize", function(){
+  const sidebar = document.querySelector(".sideMenu");
+  const mainPage = document.querySelector('.mainPage')
+
+    if ($(window).width() < 1000) {
+      sidebar.classList.add("collapsed");
+      mainPage.classList.add("collapsed")
+    } else {
+      sidebar.classList.remove("collapsed");
+      mainPage.classList.remove("collapsed")
+    }
+  }); 
